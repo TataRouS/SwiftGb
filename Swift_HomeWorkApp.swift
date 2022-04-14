cbc066d89d9559a1eda96a26558283c1b5c5e817
TataRousSmith <77956985+TataRousSmith@users.noreply.github.com>
2022-04-14 19:44:10 +0300

    Add files via upload

---
+++ Swift_HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_HomeWorkApp.swift
+//  Swift_HomeWork
+//
+//  Created by Nata Kuznetsova on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
